97bc5cd087aeceabdd4a4d5fb35088062036b6e8
Javad Amirian <amiryan.j@gmail.com>
2025-01-19 14:41:11 +0100

    Add .env file creation in install script for environment configuration

--- tm_bot/install.sh
+++ tm_bot/install.sh
@@ -8,6 +8,16 @@ echo "Installing Python and pip..."
 sudo apt-get install -y python3 python3-pip python3-venv
 sudo apt install python3.11-venv
 
+# Check if the .env file exists, if not create an empty one with placeholders
+if [ ! -f ".env" ]; then
+    echo "Creating an empty .env file with placeholders..."
+    echo "BOT_TOKEN=\"your_bot_token_here\"" > .env
+    echo "OPENAI_API_KEY=\"your_openai_api_key_here\"" >> .env
+    echo "ROOT_DIR=\"your_root_directory_here\"" >> .env
+else
+    echo ".env file already exists. Skipping creation."
+fi
+
 # Check if the virtual environment already exists
 if [ ! -d ".venv" ]; then
     # Create a virtual environment in the root folder of the repo
