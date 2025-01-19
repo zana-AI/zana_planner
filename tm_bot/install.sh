a4394e7ceea578267cac056a64d7758d5a4ccc74
Javad Amirian <amiryan.j@gmail.com>
2025-01-19 14:38:25 +0100

    add support for Python 3.11 venv installation

--- tm_bot/install.sh
+++ tm_bot/install.sh
@@ -6,10 +6,16 @@ sudo apt-get update
 
 echo "Installing Python and pip..."
 sudo apt-get install -y python3 python3-pip python3-venv
+sudo apt install python3.11-venv
 
-# Create a virtual environment in the root folder of the repo
-echo "Creating a virtual environment..."
-python3 -m venv .venv
+# Check if the virtual environment already exists
+if [ ! -d ".venv" ]; then
+    # Create a virtual environment in the root folder of the repo
+    echo "Creating a virtual environment..."
+    python3 -m venv .venv
+else
+    echo "Virtual environment already exists. Skipping creation."
+fi
 
 # Activate the virtual environment
 echo "Activating the virtual environment..."
