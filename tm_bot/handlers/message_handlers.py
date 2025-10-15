70faab3837783b79c8bcdde4a9005eb6d0f76b14
javad <amiryan.j@gmail.com>
2025-10-15 14:41:18 +0200

    detect the group and User language from telegram data

--- tm_bot/handlers/message_handlers.py
+++ tm_bot/handlers/message_handlers.py
@@ -288,8 +288,9 @@ class MessageHandlers:
         try:
             user_message = update.message.text
             user_id = update.effective_user.id
-            user_lang = get_user_language(user_id)
-            
+            user_group_id = update.effective_chat.id if update.effective_chat.type in ['group', 'supergroup'] else None
+            user_lang = get_user_language(update.effective_user)
+
             # Check if user exists, if not, call start
             user_dir = os.path.join(self.root_dir, str(user_id))
             if not os.path.exists(user_dir):
